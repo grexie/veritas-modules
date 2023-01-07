8b250de784464cdc147799bb369834d894dbbe4c
Tim Behrsin <timothy@behrsin.org>
2023-01-07 05:39:24 +0000

    Updates

---
+++ default-route/grexie-veritas.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./default-route/index.ts ***!
+  \********************************/
+console.log("hello from default route");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVmYXVsdC1yb3V0ZS9ncmV4aWUtdmVyaXRhcy5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBQSxPQUFPLENBQUNDLEdBQUcsQ0FBQywwQkFBMEIsQ0FBQyxDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vZGVmYXVsdC1yb3V0ZS9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcImhlbGxvIGZyb20gZGVmYXVsdCByb3V0ZVwiKTtcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
---
+++ domains/search/google/grexie-veritas.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************************!*\
+  !*** ./domains/search/google/index.ts ***!
+  \****************************************/
+console.log("hello world from google at grexie-veritas");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZG9tYWlucy9zZWFyY2gvZ29vZ2xlL2dyZXhpZS12ZXJpdGFzLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBRyxDQUFDLDJDQUEyQyxDQUFDLEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9kb21haW5zL3NlYXJjaC9nb29nbGUvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJoZWxsbyB3b3JsZCBmcm9tIGdvb2dsZSBhdCBncmV4aWUtdmVyaXRhc1wiKTtcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
